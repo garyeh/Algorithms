9182ec44f0980bcdeb287f672303a6c403262c2a
Gary Yeh <garyeh@gmail.com>
2017-06-17 18:59:15 -0700

    Implement insert and find on BST

--- BinarySearchTree/lib/binary_search_tree.rb
+++ BinarySearchTree/lib/binary_search_tree.rb
@@ -1,14 +1,31 @@
-# There are many ways to implement these methods, feel free to add arguments 
+# There are many ways to implement these methods, feel free to add arguments
 # to methods as you see fit, or to create helper methods.
+require_relative 'bst_node'
 
 class BinarySearchTree
+  attr_reader :root
+
   def initialize
+    @root = nil
   end
 
   def insert(value)
+    if root
+      deeper_insert(value, @root)
+    else
+      @root = BSTNode.new(value)
+      value
+    end
   end
 
   def find(value, tree_node = @root)
+    return nil unless tree_node
+    return tree_node if tree_node.value == value
+    if value > tree_node.value
+      find(value, tree_node.right)
+    elsif value < tree_node.value
+      find(value, tree_node.left)
+    end
   end
 
   def delete(value)
@@ -19,7 +36,7 @@ class BinarySearchTree
   end
 
   def depth(tree_node = @root)
-  end 
+  end
 
   def is_balanced?(tree_node = @root)
   end
@@ -29,6 +46,22 @@ class BinarySearchTree
 
 
   private
-  # optional helper methods go here:
+  def deeper_insert(value, root)
+    if value > root.value
+      if root.right
+        deeper_insert(value, root.right)
+      else
+        root.right = BSTNode.new(value)
+        value
+      end
+    elsif value < root.value
+      if root.left
+        deeper_insert(value, root.left)
+      else
+        root.left = BSTNode.new(value)
+        value
+      end
+    end
+  end
 
 end
