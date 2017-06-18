5e34bd8b88f44c98204a31b292e7bb806fb3d130
Gary Yeh <garyeh@gmail.com>
2017-06-18 08:56:09 -0700

    Finish kth largest problem

--- BinarySearchTree/lib/bst_practical.rb
+++ BinarySearchTree/lib/bst_practical.rb
@@ -1,5 +1,13 @@
 require_relative 'binary_search_tree'
 
 def kth_largest(tree_node, k)
-  
+  reverse_traversal(tree_node, k)
+end
+
+def reverse_traversal(tree_node, k, max = [])
+  reverse_traversal(tree_node.right, k, max) if tree_node.right
+  max << tree_node
+  reverse_traversal(tree_node.left, k, max) if tree_node.left
+
+  max[k-1]
 end
